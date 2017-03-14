ccbf679cc0ac9970aacd28eda0355bf14915f9cc
[daoket] <[daoket@163.com]>
2017-03-14 23:21:54 +0800

    .

--- src/components/News/News.vue
+++ src/components/News/News.vue
@@ -1,37 +1,69 @@
 <template>
   <div class="news">
-    <span>news lalalalla</span>
+    <div class="nav">
+      <ul>
+      	<li v-for='it in item' @click='clickLi'>{{it.title}}</li>
+      </ul>
+    </div>
+    <div class="main">
+      <h2>习近平五下上海团五谈创新</h2>
+      <p>2017/3/14 上午8:58:06</p>
+    </div>
   </div>
 </template>
 
 <script>
+import $ from 'jquery'
 export default {
   name: 'news',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      item: [{
+        title: '推荐'
+      }, {
+        title: '要闻'
+      }, {
+        title: '财经'
+      }, {
+        title: '科技'
+      }]
+    }
+  },
+  methods: {
+    clickLi (e) {
+      $(e.target).addClass('active').siblings().removeClass('active')
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+<style lang="stylus">
+.news{
+  padding-top: 60px;
+  display: flex;
+  justify-content: flex-start;
+  .nav{
+    height: 80%;
+    width: 150px;
+    text-align: center;
+    border-right: 5px solid #B44847;
+    li{
+      height: 45px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      cursor: pointer;
+      font-size: 20px;
+    }
+    .active{
+      color: #fff;
+      background: #ED4040;
+      border-radius: 4px;
+      margin: 0 20px;
+    }
+  }
+  .main{
+    padding: 12px;
+  }
 }
 </style>
