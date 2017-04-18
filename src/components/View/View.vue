c0b117773560a3a996a15acdf0e4ee0d3cb59198
daoket <daoket@163.com>
2017-04-19 00:30:20 +0800

    .

--- src/components/View/View.vue
+++ src/components/View/View.vue
@@ -20,24 +20,23 @@ export default {
     }
   },
   created () {
-    let [page, flag] = [0, true]
-    var url = 'https://api.unsplash.com/photos?client_id=80f66654628683dc7a20a3f2b44a93f8a9f0afaa41be7c7c392c5648dc6bb035&page='
-    setInterval(function () {
-      if (document.body.scrollHeight <= window.innerHeight && flag) {
-        askData(url)
+    let page = 1
+    let url = 'https://api.unsplash.com/photos?client_id=80f66654628683dc7a20a3f2b44a93f8a9f0afaa41be7c7c392c5648dc6bb035&page='
+    askData(url + page)
+    window.onscroll = function () {
+      var leaveBottom = (document.body.scrollHeight - document.body.scrollTop - window.innerHeight)
+      if (leaveBottom < 1) {
+        page++
+        askData(url + page)
       }
-    }, 500)
+    }
     function askData (url) {
-      page++
       $.ajax({
         type: 'get',
-        url: url + page,
+        url: url,
         async: true,
         success: function (res) {
           $('.load').hide()
-          if (res.length === 0) {
-            flag = false
-          }
           loadImg(res)
         },
         error: function (err) {
@@ -48,9 +47,34 @@ export default {
     function loadImg (data) {
       for (let s of data) {
         var html = `<div class="box">
-                    <img src="${s.urls.small}"/>
-                  </div>`
+                      <img src="${s.urls.small}"/>
+                    </div>`
         $(html).appendTo('.view .main')
+      }
+      imgPositon()
+    }
+    function imgPositon () {
+      let topArr = []
+      let allImg = $('.view .main .box')
+      let columns = parseInt(($(window).width() - 200) / 300)
+      for (var i = 0; i < allImg.length; i++) {
+        if (i < columns) {
+          $(allImg[i]).css({
+            'top': 0,
+            'left': i % columns * 305 + 'px'
+          })
+          topArr[i] = $(allImg[i]).height()
+        } else {
+          console.log(topArr)
+          let min = Math.min.apply(null, topArr)
+          let num = topArr.indexOf(min)
+          topArr[num] = topArr[num] + 5
+          $(allImg[i]).css({
+            'top': topArr[num] + 'px',
+            'left': i % columns * 305 + 'px'
+          })
+          topArr[num] = topArr[num] + $(allImg[i]).height()
+        }
       }
     }
     setTimeout(function () {
@@ -71,11 +95,15 @@ export default {
 <style lang="stylus">
 .view{
   text-align: center;
-  .box{
-    padding: 1px;
-    display: inline-block;
-    img{
-      width: 300px;
+  .main{
+    position: relative;
+    .box{
+      padding: 1px;
+      position: absolute;
+      display: inline-block;
+      img{
+        width: 300px;
+      }
     }
   }
   .loadMore{
