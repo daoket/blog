88cefbbeed378f2753add0da14b372477b612c68
daoket <daoket@163.com>
2017-04-18 23:40:40 +0800

    添加图标

--- src/components/Menus/Menus.vue
+++ src/components/Menus/Menus.vue
@@ -6,7 +6,12 @@
     </div>
     <ul class="aside">
     	<li v-for='todo in todos'>
-    	  <router-link :to='setPath(todo.path)'>{{todo.text}}</router-link>
+    	  <router-link :to='setPath(todo.path)'>
+    	    <svg class="icon" aria-hidden="true">
+            <use :xlink:href='setXlink(todo.xlink)'></use>
+          </svg>
+    	    <span>{{todo.text}}</span>
+    	  </router-link>
     	</li>
     </ul>
   </div>
@@ -19,31 +24,41 @@ export default {
     return {
       todos: [{
         text: '首页',
-        path: '/welcome'
+        path: '/welcome',
+        xlink: '#icon-shouye'
       }, {
         text: '故事',
-        path: '/story'
+        path: '/story',
+        xlink: '#icon-gushi'
       }, {
         text: '音乐',
-        path: '/music'
+        path: '/music',
+        xlink: '#icon-yinle'
       }, {
         text: '新闻',
-        path: '/news'
+        path: '/news',
+        xlink: '#icon-xinwen'
       }, {
         text: '电影',
-        path: '/movie'
+        path: '/movie',
+        xlink: '#icon-dianying'
       }, {
         text: '风景',
-        path: '/view'
+        path: '/view',
+        xlink: '#icon-fengjing'
       }, {
         text: '关于我',
-        path: '/me'
+        path: '/me',
+        xlink: '#icon-guanyuwo'
       }]
     }
   },
   methods: {
     setPath (path) {
       return path
+    },
+    setXlink (xlink) {
+      return xlink
     }
   }
 }
@@ -88,12 +103,27 @@ export default {
       height: 100%;
       width: 100%;
       display: flex;
+      font-size: 20px;
       justify-content: center;
       align-items: center;
+      span{
+        margin:  0 15px;
+      }
     }
     a:hover{
       color: #fff;
     }
   }
+}
+.icon {
+  /* 通过设置 font-size 来改变图标大小 */
+  width: 25px; height: 25px;
+  /* 图标和文字相邻时，垂直对齐 */
+  vertical-align: -0.15em;
+  /* 通过设置 color 来改变 SVG 的颜色/fill */
+  fill: currentColor;
+  /* path 和 stroke 溢出 viewBox 部分在 IE 下会显示
+     normalize.css 中也包含这行 */
+  overflow: hidden;
 }
 </style>
